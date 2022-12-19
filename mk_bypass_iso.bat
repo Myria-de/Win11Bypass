216849e13b5103c805d55b1ad4595c56c8b51308
Thorsten Eggeling <Myria-de@users.noreply.github.com>
2022-12-19 19:11:10 +0000

    Update. Some error handling.

--- mk_bypass_iso.bat
+++ mk_bypass_iso.bat
@@ -1,38 +1,75 @@
 @echo off
 setlocal
+REM German
+chcp 1252
+REM 
+REM Version 1.2
+REM 
+openfiles > NUL 2>&1 
+if NOT %ERRORLEVEL% EQU 0 goto NotAdmin 
+goto StartMain
+:NotAdmin 
+echo Sie müssen das Script als Administrator starten
+goto error
+
+:StartMain
 if /i "%~1"=="" goto usage
 SET ISOFILENAME=%~1
+
+REM Bei Bedarf anpassn
+SET VOLID="Win11"
+SET ISONAME="Windows_11_bypass.iso"
 
 if not exist "ISO" mkdir ISO
 if not exist "WIM" mkdir WIM
 
+if exist "ISO\sources\boot.wim" (
+echo "ISO\sources\boot.wim ist bereits vorhanden."
+echo "Bitte löschen Sie das Verzeichnis ISO."
+goto end
+)
+
 echo ISO extrahieren
 7z x -y %ISOFILENAME% -oISO
-echo boot.wim einhaengen
+if %errorlevel% neq 0 goto error
+
+
+echo boot.wim einhängen
 dism /Mount-Image /Imagefile:"ISO\sources\boot.wim" /Index:2 /MountDir:WIM
+if %errorlevel% neq 0 goto error
 echo Registry-Hive laden
 reg load HKLM\Z "WIM\Windows\System32\config\SYSTEM"
 echo Registry-Import
 reg import bypass.reg
 echo Registry-Hive unload
 reg unload HKLM\Z
-echo Aenderungen in boot.wim speichern
+echo Änderungen in boot.wim speichern
 Dism /Unmount-image /MountDir:WIM /Commit
+REM Aufräumen
 dism /Get-MountedImageInfo
 dism /cleanup-mountpoints
 dism /cleanup-wim
 dism /Get-MountedImageInfo
 :mkiso
-mkisofs.exe -iso-level 4 -udf -r -force-uppercase -duplicates-once -volid "Win11" -hide boot.catalog -hide-udf boot.catalog -b "boot/etfsboot.com" -no-emul-boot -boot-load-size 8 -eltorito-platform efi -b "efi/microsoft/boot/efisys.bin" -no-emul-boot -o Windows_11_bypass.iso ISO
+mkisofs.exe -iso-level 4 -udf -r -force-uppercase -duplicates-once -volid %VOLID% -hide boot.catalog -hide-udf boot.catalog -b "boot/etfsboot.com" -no-emul-boot -boot-load-size 8 -eltorito-platform efi -b "efi/microsoft/boot/efisys.bin" -no-emul-boot -o %ISONAME% ISO
 
-goto end
+goto endok
 
 :usage
 echo Erstellt ISO-Datei fuer Windows 11 mit Hardware-Bypass
 echo.
 echo mk_bypass_iso.bat ISO-Datei
 echo.
+goto end
+
+:endok
+echo Fertig
+goto end
+:error
+REM aufräumen
+echo "Abbruch: Es ist ein Fehler aufgetreten: %errorlevel%"
+dism /cleanup-mountpoints
+dism /cleanup-wim
 
 :end
-echo Fertig
 endlocal
